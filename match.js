39b293052cec7a678c98e83551b8bf615b3c9171
AM\Lufteluke <lufteluke@corrupted.systems>
2019-09-08 02:30:03 +0200

    more links

--- match.js
+++ match.js
@@ -6,7 +6,7 @@ module.exports.pairs = [
     ["linux", "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX. Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called \"Linux\", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project. There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called \"Linux\" distributions are really distributions of GNU/Linux."],
     ["bad dragon", "OwO whats this that you just said about me, you little bitch? I’ll have you know I graduated top of my class in art school, and I’ve been involved in numerous convention hotel room orgies, and I have over 300 confirmed gay yiff commissions. I am twained in taking big gorilla daddy dick and I’m the top cumguzzler in the entire furry community. You are nothing to me but just another femboy fox. I will suck you the fuck off with fur-y the likes of which has never been seen before on this Earth, mark my fur-cking words. You think you can get away with saying that scat to me over furrymate.com? Think again, fur-cker. As we speak I am contacting my secwet network of big dick daddies across the USA and your IP is being traced right now so you better prepare for the stowm, faggot. The stowm that licks and kisses the pathetic little thing you call your bulge. You’re getting fucking head, kid. I can be anywhere, anytime, and I can pounce on you in over seven hundred ways, and that’s just with my bare paws. Not only am I extensively trained in unarmed gay yiff, but I have access to the entire arsenal of baddragon.com and I will use it to its full extent to fuck your misewable ass off the face of the continent, you wittle shit. If only you could have known what unholy bulgy wulgy your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you could knot, you did knot, and now you’re paying the price, you goddamn idiot. I will shit cum all over you and you will drown in it. Your fragile heterosexuality is fucking dead, cubbo."],
     ["babby formed", "They need to do way instain mother> who kill thier babbys, becuse these babby cant fright back? It was on the news this mroing a mother in ar who had kill her three kids, they are taking the three babby back to new york too lady to rest. my pary are with the father who lost his chrilden ; i am truley sorry for your lots. https://youtu.be/Ll-lia-FEIY"],
-    ["pragnent", "They need to do way instain mother> who kill thier babbys, becuse these babby cant fright back? It was on the news this mroing a mother in ar who had kill her three kids, they are taking the three babby back to new york too lady to rest. my pary are with the father who lost his chrilden ; i am truley sorry for your lots. https://youtu.be/Ll-lia-FEIY"],
+    
 
 
     //links
@@ -16,10 +16,11 @@ module.exports.pairs = [
     ["drink the water",  "Excuse me this is important: https://www.youtube.com/watch?v=1auCmplVCsY"],
     ["bodywash", "POOOOWEEEEER https://youtu.be/XpJvLd2H65U"],
     ["old spice", "POOOOWEEEEER https://youtu.be/XpJvLd2H65U"],
+    ["pragnent", "https://www.youtube.com/watch?v=EShUeudtaFg"],
     ["shantae", "POOOOWEEEEER https://youtu.be/XpJvLd2H65U"],
     ["octagon", "https://youtu.be/7ziWe_oNb58"],
     ["cash money", "https://youtu.be/TWaP-RnGocg"],
-    ["josh", "Hey Josh, look at this huge moth that I found https://www.youtube.com/watch?v=B211aVOdvpc"],
+    ["moth", "Hey Josh, look at this huge moth that I found https://www.youtube.com/watch?v=B211aVOdvpc"],
     ["stew", "https://youtu.be/HnyGSl3K-IE"],
     ["too hot", "https://youtu.be/XhIBegli6dE"],
     ["tranquility", "https://youtu.be/SHvhps47Lmc"],
@@ -39,13 +40,36 @@ module.exports.pairs = [
     ["safri duo", "https://m.youtube.com/watch?v=NuTPK1veaQA"],
     ["eguegu", "https://www.youtube.com/watch?v=3-rfBsWmo0M"],
     ["windows", "https://www.youtube.com/watch?v=AGHXao3Eu64"],
+    ["furby", "https://www.youtube.com/watch?v=GYLBjScgb7o"],
+    ["no money", "https://youtu.be/AWGt2AOP2bs"],
+    ["state nightmare", "https://www.youtube.com/watch?v=CqORW1DsyX0"],
+    ["eraser", "Because I am a good eraser https://youtu.be/1kJwpINVfo8"],
+    ["erased", "There are many things that need to be erased https://youtu.be/efjjmpOVzRg?t=135"],
+    ["warcrime", "https://youtu.be/aY1XpTzLn0A"],
+    ["kebab", "https://youtu.be/q9HzqxwKfiM"],
+    ["dream", "https://www.youtube.com/watch?v=4bHww53BaLg"],
+    ["cinco", "https://www.youtube.com/watch?v=TLa1jlhEtu8"],
+    ["konata", "https://youtu.be/9gvswd1I8qo"],
+    ["rapist", "What's worse than a rapist? https://www.youtube.com/watch?v=bfCR0dEDO1A"],
+    ["6502", "https://www.youtube.com/watch?v=HDMkw6lnzmI"],
+    ["scout", "https://youtu.be/24Eh2-DZTgQ"],
+    ["reality", "https://youtu.be/fV3nflAQ99w"],
+    ["you can do it", "https://www.youtube.com/watch?v=rCvtIdLxu1Y"],
+    ["spaghetti", "https://youtu.be/SW-BU6keEUw"],
+    ["king", "https://www.youtube.com/watch?v=w_MSFkZHNi4"],
+    ["sharing", "https://youtu.be/VTlB9ibTMSo?t=49"],
+    ["crab", "https://www.youtube.com/watch?v=7ktHrtxUHbg"],
+    ["luigi", "https://www.youtube.com/watch?v=15nNY7uofNw"],
+    ["brody", "https://www.youtube.com/watch?v=YqBLxzn6kcU"],
+    ["limewire", "https://www.youtube.com/watch?v=IBH4g_ua5es"],
+    ["toy food", "https://youtu.be/gVvhxwtFsnE"],
+    ["cooking", "https://www.youtube.com/watch?v=K5tVbVu9Mkg"],
 
     
     //things he don't like
     ["botter", "Hey hey hey, don't use the b word"],
     ["eat shit", "and die"],
     ["objection", "Sustained!"],
-    ["interject", "Objection! https://youtu.be/W8QRExBfQhs"],
 
     //corrections
     ["magpie", "*Piemag"],
@@ -55,7 +79,8 @@ module.exports.pairs = [
     ["hedgehog", "*Hodgehedge"],
     ["coyote", "*Yoot"],
     ["youre", "*you're"],
-    ["breathtaking", "You're breathtaking"],
+    ["breathtaking", "You're breathtaking!"],
+    ["", "good bot"],
 
     //things he likes
     ["jackdaw", "Jackdaws are precious."],    
@@ -63,6 +88,7 @@ module.exports.pairs = [
     ["lufbot", "I love @Lufbot!"],
     ["interjectionbot", "Hey, that's me!"],
     ["brainfuck", "I love @BrainfuckingBot"],
+    ["interject", "Objection! https://youtu.be/W8QRExBfQhs"],
     
     //baseline test
     ["baseline", "And blood-black nothingness began to spin... A system of cells interlinked within cells interlinked within cells interlinked within one stem... And dreadfully distinct against the dark, a tall white fountain played."],
@@ -82,6 +108,9 @@ module.exports.pairs = [
     ["bleep", "bloop"],
     ["shrug", "¯\\_(ツ)_/¯"],
 
+    ["", ""],
+    ["", ""],
+    ["", ""],
     ["", ""],
     ["", ""],
     ["", ""],
