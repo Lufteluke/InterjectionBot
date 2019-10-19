0a1d786af10c3cc8fbc4152588097e9ac425bd00
AM\Lufteluke <lufteluke@corrupted.systems>
2019-10-19 03:28:41 +0200

    removed debug and added ignore array back

--- sentenceAnalyser.js
+++ sentenceAnalyser.js
@@ -19,9 +19,8 @@ module.exports.parseString = function (string) {
     let rule = match.pairs
 
     for (let i = 0; i < rule.length; i++) {
-        //console.log(i + ": " + rule[i][1])
         if (!ignoreArray[i] && matchList(rule[i][0], string, false)) {
-            //console.log("Correct: (" + i+ ")" + JSON.stringify(rule[i][1]))
+            ignoreArray[i] = true
             return rule[i][1]
         }
     }
@@ -32,18 +31,13 @@ function matchList(match, src, matchAll) {
     let retVal = matchAll
     
     if (Array.isArray(match)) {
-        //console.log("Array start " + (matchAll? "(all)" : "(any)") )
         for (let j = 0; j < match.length; j++) {
-            //console.log("Array item start " + (matchAll? "(all): ":"(any): ") + JSON.stringify(match[j]))
             let check = matchList(match[j], src, !matchAll)
             retVal = (retVal && check) || (!matchAll && (check || retVal))
-            //console.log("Retval: " + retVal, ". Matchall: " + matchAll)
         }
-        //console.log("Array end: " + retVal)
         return retVal
     }
     else {
-        //console.log(src + " > " + match + " " + src.includes(match))
-        return src.includes(match);
+        return src.includes(match)
     }
 }
